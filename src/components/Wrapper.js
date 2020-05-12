63aaa532e65b2e33777a32ab116b9838ed0c7d31
Louis Coleman <coleloui18@gmail.com>
2020-05-12 14:13:08 -0700

    main

---
+++ src/components/Wrapper.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Wrapper(props) {
+  return <div className="lg:m-8" {...props} />;
+}
+
+export default Wrapper;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,41 +3,79 @@ import React from "react";
 function Main() {
   return (
     <div className="grid grid-cols-12 gap-4">
-      <div className="col-span-3">
+      <div className="col-span-2">
         <div className="grid invisible lg:visible">
           <div className="container rounded border-2 border-RocketRed">
-            <h1>Top categories</h1>
-            <h1>Top categories</h1>
-            <h1>Top categories</h1>
-            <h1>Top categories</h1>
-            <h1>Top categories</h1>
+            <h1 className="text-center">Top categories</h1>
+            {/* make this into a list */}
+            <div className="ml-4 mb-1">
+              <p>Top categories</p>
+              <p>Top categories</p>
+              <p>Top categories</p>
+              <p>Top categories</p>
+            </div>
           </div>
           <br></br>
           <div className="container rounded border-2 border-RocketJames">
-            <h1>All categories</h1>
+            <h1 className="text-center">All categories</h1>
+            {/* make this into a list */}
+            <div className="ml-4 mb-1">
+              <p>Category</p>
+              <p>Category</p>
+              <p>Category</p>
+              <p>Category</p>
+              <p>Category</p>
+              <p>Category</p>
+              <p>Category</p>
+              <p>Category</p>
+              <p>Category</p>
+              <p>Category</p>
+              <p>Category</p>
+              <p>Category</p>
+              <p>Category</p>
+            </div>
           </div>
         </div>
       </div>
-      <div className="col-span-6">
-        <h1>test</h1>
+      <div className="col-span-8">
+        <h1 className="text-center">placeholder for posts</h1>
       </div>
-      <div className="col-span-3">
+      <div className="col-span-2">
         <div className="grid invisible lg:visible">
           <div className="container rounded border-2 border-RocketJessie">
-            <h1>Top categories</h1>
-            <h1>Top categories</h1>
-            <h1>Top categories</h1>
-            <h1>Top categories</h1>
+            <h1 className="text-center">Top Point Holders</h1>
+            {/* make this into a list */}
+            <div className="ml-4 mb-1">
+              <p>Rory</p>
+              <p>Marlon</p>
+              <p>Paul</p>
+              <p>Dion</p>
+              <p>Louis</p>
+            </div>
           </div>
           <br></br>
           <div className="container rounded border-2 border-RocketRed">
-            <h1>All categories</h1>
+            <h1 className="text-center">Top Posters</h1>
+            {/* make this into a list */}
+            <div className="ml-4 mb-1">
+              <p>Rory</p>
+              <p>Marlon</p>
+              <p>Paul</p>
+              <p>Dion</p>
+              <p>Louis</p>
+            </div>
           </div>
           <br></br>
           <div className="container rounded border-2 border-RocketRed">
-            <h1>All categories</h1>
-            <h1>All categories</h1>
-            <h1>All categories</h1>
+            <h1 className="text-center">Mods</h1>
+            {/* make this into a list */}
+            <div className="ml-4 mb-1">
+              <p>Rory</p>
+              <p>Marlon</p>
+              <p>Paul</p>
+              <p>Dion</p>
+              <p>Louis</p>
+            </div>
           </div>
         </div>
       </div>
