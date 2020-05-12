6ea1d431bd9a34d1abbe1596a7794ac4395d2a7c
NA Flipper <dioncleung@gmail.com>
2020-05-12 11:13:52 -0700

    renamed main.js

--- src/pages/main.js
+++ src/pages/main.js
@@ -4,8 +4,8 @@ function Main() {
   return (
     <div className="grid grid-cols-12 gap-4">
       <div className="col-span-3">
-        <div className="grid invisible lg:visible">
-          <div className="container rounded border-2">
+        <div className="grid grid-rows-6">
+          <div className="container rounded border-2 row-span-2">
             <h1>Top categories</h1>
             <h1>Top categories</h1>
             <h1>Top categories</h1>
@@ -13,7 +13,7 @@ function Main() {
             <h1>Top categories</h1>
           </div>
           <br></br>
-          <div className="container rounded border-2">
+          <div className="container row-span-4 rounded border-2">
             <h1>All categories</h1>
           </div>
         </div>
@@ -22,24 +22,7 @@ function Main() {
         <h1>test</h1>
       </div>
       <div className="col-span-3">
-        <div className="grid invisible lg:visible">
-          <div className="container rounded border-2">
-            <h1>Top categories</h1>
-            <h1>Top categories</h1>
-            <h1>Top categories</h1>
-            <h1>Top categories</h1>
-          </div>
-          <br></br>
-          <div className="container rounded border-2">
-            <h1>All categories</h1>
-          </div>
-          <br></br>
-          <div className="container rounded border-2">
-            <h1>All categories</h1>
-            <h1>All categories</h1>
-            <h1>All categories</h1>
-          </div>
-        </div>
+        <h1>test</h1>
       </div>
     </div>
   );
