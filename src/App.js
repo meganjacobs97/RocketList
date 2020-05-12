a33d189fd4741f4424c12e0098e693ac0d44e7b7
NA Flipper <dioncleung@gmail.com>
2020-05-12 01:05:28 -0700

    outlined class based component for category view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import RocketList from './components/Rocketlist';
+// import RocketList from './components/Rocketlist';
+// import Navbar from './components/Navbar';
+import CategoryView from './components/CategoryView';
 
 function App() {
   return (
-    <RocketList />
+    // <Navbar />
+    // <RocketList />
+    <CategoryView />
   );
 }
 
---
+++ src/components/CategoryView.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+// import { connect } from 'react-redux'
+
+export class CategoryView extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a category</h1>
+      </div>
+    )
+  }
+}
+
+// const mapStateToProps = (state) => ({
+  
+// })
+
+// const mapDispatchToProps = {
+  
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CategoryView)
+export default CategoryView
+
+// check class repo, week 10, folder 19, activity 15 for class based components
