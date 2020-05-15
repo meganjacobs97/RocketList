bbce312368a62a6097e44e675d8292e528947aa0
Louis <coleloui18@gmail.com>
2020-05-15 10:38:35 -0700

    moving to branch

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import R from "./Photo/R.png";
 
 export default function Hamburger() {
+  const [isOpen, setIsOpen] = useState(true)
+  
   return (
     <div>
-      <div>
+      <div className="relative">
         <button className="block">
-          <img src={R} alt="avatar"></img>
+          <img src={R} alt="drop down menu"></img>
         </button>
-      </div>
-      <div>
-        <a href="#">account settings</a>
-        <a href="#">support</a>
-        <a href="#">logout</a>
+        <div v-if="isOpen" className="absolute right-0 mt-2 w-48 py-2 bg-white rounded-lg shadow-md">
+          <a
+            href="#"
+            className="block px-4 py-2 text-RocketBlack hover:bg-RocketMeowth"
+          >
+            support
+          </a>
+          <a
+            href="#"
+            className="block px-4 py-2 text-RocketBlack hover:bg-RocketMeowth"
+          >
+            account settings
+          </a>
+          <a
+            href="#"
+            className="block px-4 py-2 text-RocketBlack hover:bg-RocketMeowth"
+          >
+            logout
+          </a>
+        </div>
       </div>
     </div>
   );
