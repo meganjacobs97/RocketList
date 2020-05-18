314fd6cbb9175e89e6e784d6bd691d9d29f691e3
vb27 <vampblood87@gmail.com>
2020-05-18 11:43:45 -0700

    wip

--- src/components/InfoBar.js
+++ src/components/InfoBar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
+function back(){
+  window.history.go(-2)
+}
+
 const InfoBar = ({ room }) => (
   <div className="flex item-center justify-between bg-RocketRed h-16 w-full rounded-t-lg">
     <div className="flex item-center ml-5p text-white">
       <h3 className="pt-4">Chat Room: ({room})</h3>
     </div>
     <div className="flex justify-end mr-5p pt-4 underline text-white hover:text-black">
-      {/* change this route to send it back to post */}
-      <a href="/">Back to Post</a>
+      <button onClick={() => {back()}}>test</button>
     </div>
   </div>
 );
