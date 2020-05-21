a82a1b86d3d7cf92d16f85e5068011ccdb3d9ec1
NA Flipper <dioncleung@gmail.com>
2020-05-21 10:24:47 -0700

    fixed key error in unordered list, fixed link and href errors

--- src/components/UnorderedList.js
+++ src/components/UnorderedList.js
@@ -25,7 +25,7 @@ function UnorderedList(props) {
       <ul className="list-none list-inside ml-4 mr-4 mb-1 text-center">
         {props.list ? (
           props.list.map((item) => (
-            <Link to={`/profile/${item.id}`} >
+            <Link key={item.id} to={`/profile/${item.id}`} >
             <li
               className="state-rendered-item"
               id={item.id}
