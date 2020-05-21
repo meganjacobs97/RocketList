6803eccb387643eda2d3d610f4d33378988e3cfe
Megan Jacobs <meganjacobs@live.com>
2020-05-21 13:01:59 -0700

    sorting points and posters

--- src/pages/CategoryView.js
+++ src/pages/CategoryView.js
@@ -171,7 +171,15 @@ function CategoryView(props) {
       setTopPoints({
         ...topPoints,
         title: "Top Points Holders",
-        topPoints: topPointsData.users.map((user) => ({
+        topPoints: topPointsData.users.sort(function(a,b){
+          if(a.points > b.points) { 
+            return -1; 
+          }
+          else if(a.points < b.poins) {
+              return 1; 
+          }
+          return 0;
+        }).map((user) => ({
           name: user.username,
           id: user._id,
           points: user.points
@@ -191,7 +199,15 @@ function CategoryView(props) {
       setTopPosters({
         ...topPosters,
         title: "Top Posters",
-        topPosters: topPostersData.users.map((user) => ({
+        topPosters: topPostersData.users.sort(function(a,b){
+          if(a.posts.length > b.posts.length) { 
+            return -1; 
+          }
+          else if(a.posts.length < b.posts.length) {
+              return 1; 
+          }
+          return 0;
+        }).map((user) => ({
           name: user.username,
           id: user._id,
           posts: user.posts.length
