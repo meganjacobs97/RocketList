f9195abbf1aa89d1b4dbec1ecdff9e36cd497575
Megan Jacobs <meganjacobs@live.com>
2020-05-21 09:57:13 -0700

    clicking on mods now takes to profile page

--- src/App.js
+++ src/App.js
@@ -56,6 +56,10 @@ const App = () => {
             {" "}
             <AccountPage UserId={UserId}></AccountPage>
           </Route>
+          <Route path={`/profile/:userId`}>
+            {" "}
+            <AccountPage></AccountPage>
+          </Route>
           <Route path="/join/:id" exact component={Join} />
           <Route path="/chat" component={Chat} />
           <Route path="*" component={NoMatch} />
