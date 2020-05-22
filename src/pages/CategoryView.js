3820c4e2165bfa0fed5dc1703ab70aef108912c4
Megan Jacobs <meganjacobs@live.com>
2020-05-21 20:49:33 -0700

    removed sorting bug

--- src/pages/CategoryView.js
+++ src/pages/CategoryView.js
@@ -171,15 +171,7 @@ function CategoryView(props) {
       setTopPoints({
         ...topPoints,
         title: "Top Points Holders",
-        topPoints: topPointsData.users.sort(function(a,b){
-          if(a.points > b.points) { 
-            return -1; 
-          }
-          else if(a.points < b.poins) {
-              return 1; 
-          }
-          return 0;
-        }).map((user) => ({
+        topPoints: topPointsData.users.map((user) => ({
           name: user.username,
           id: user._id,
           points: user.points
@@ -199,15 +191,7 @@ function CategoryView(props) {
       setTopPosters({
         ...topPosters,
         title: "Top Posters",
-        topPosters: topPostersData.users.sort(function(a,b){
-          if(a.posts.length > b.posts.length) { 
-            return -1; 
-          }
-          else if(a.posts.length < b.posts.length) {
-              return 1; 
-          }
-          return 0;
-        }).map((user) => ({
+        topPosters: topPostersData.users.map((user) => ({
           name: user.username,
           id: user._id,
           posts: user.posts.length
