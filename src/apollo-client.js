2bec43a8a4cbe88a960d944d5c6325729fb31a14
Megan Jacobs <meganjacobs@live.com>
2020-05-28 14:28:01 -0700

    began user authentication

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -1,6 +1,19 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
+import { setContext } from 'apollo-link-context';
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
 
 const client = new ApolloClient({
   link: createHttpLink({
